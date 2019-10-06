f6b140d01945808316da458390988827d6d9a29e
ojooss <>
2019-10-06 09:56:51 +0200

    added new register to sample.php

--- sample.php
+++ sample.php
@@ -5,8 +5,10 @@ require_once __DIR__.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.
 $ipAddress = '192.168.21.5';
 
 $ventilationUnit = new \PluggitApi\VentilationUnit($ipAddress, 'de');
+
 echo PHP_EOL;
 echo "--- PluggitApi ---".PHP_EOL;
+echo PHP_EOL;
 
 echo "Current date and time: ".$ventilationUnit->getCurrentDateTime(true).' ('.$ventilationUnit->getCurrentDateTime().')'.PHP_EOL;
 echo "Start exploitation date: ".$ventilationUnit->getStartExploitationDate(true).' ('.$ventilationUnit->getStartExploitationDate().')'.PHP_EOL;
@@ -15,17 +17,36 @@ echo "Outdoor temperature: ".$ventilationUnit->getOutdoorTemperature(true).' ('.
 echo "Supply temperature: ".$ventilationUnit->getSupplyTemperature(true).' ('.$ventilationUnit->getSupplyTemperature().')'.PHP_EOL;
 echo "Extract temperature: ".$ventilationUnit->getExtractTemperature(true).' ('.$ventilationUnit->getExtractTemperature().')'.PHP_EOL;
 echo "Exhaust temperature: ".$ventilationUnit->getExhaustTemperature(true).' ('.$ventilationUnit->getExhaustTemperature().')'.PHP_EOL;
-
-echo "Actual program: ".$ventilationUnit->getCurrentProgram(true).' ('.$ventilationUnit->getCurrentProgram().')'.PHP_EOL;
-
 echo "Fan-1 speed: ".$ventilationUnit->getFanSpeed1(true).' ('.$ventilationUnit->getFanSpeed1().')'.PHP_EOL;
 echo "Fan-2 speed: ".$ventilationUnit->getFanSpeed2(true).' ('.$ventilationUnit->getFanSpeed2().')'.PHP_EOL;
 echo "Fan speed level: ".$ventilationUnit->getFanSpeedLevel(true).' ('.$ventilationUnit->getFanSpeedLevel().')'.PHP_EOL;
-echo "Filter remaining time: ".$ventilationUnit->getFilterRemainingTime(true).' ('.$ventilationUnit->getFilterRemainingTime().')'.PHP_EOL;
-
+echo "Filter default time: ".$ventilationUnit->getFilterDefaultTime(true),' ('.$ventilationUnit->getFilterDefaultTime().')'.PHP_EOL;
+echo "Filter remaining time: ".$ventilationUnit->getFilterDefaultTime(true).' ('.$ventilationUnit->getFilterDefaultTime().')'.PHP_EOL;
 echo "Bypass state: ".$ventilationUnit->getBypassState(true).' ('.$ventilationUnit->getBypassState().')'.PHP_EOL;
 echo "Bypass temperature min: ".$ventilationUnit->getBypassTemperatureMin(true).' ('.$ventilationUnit->getBypassTemperatureMin().')'.PHP_EOL;
 echo "Bypass temperature max: ".$ventilationUnit->getBypassTemperatureMax(true).' ('.$ventilationUnit->getBypassTemperatureMax().')'.PHP_EOL;
 echo "Bypass manual timeout: ".$ventilationUnit->getBypassManualTimeout(true).' ('.$ventilationUnit->getBypassManualTimeout().')'.PHP_EOL;
+echo "Preheater duty cycle: ".$ventilationUnit->getPreheaterDutyCycle(true).' ('.$ventilationUnit->getPreheaterDutyCycle().')'.PHP_EOL;
+echo "Unit mode: ".$ventilationUnit->getUnitMode(true).' ('.$ventilationUnit->getUnitMode().')'.PHP_EOL;
+echo "CurrentBLState: ".$ventilationUnit->getCurrentBLState(true).' ('.$ventilationUnit->getCurrentBLState().')'.PHP_EOL;
+
+echo PHP_EOL;
+
+$oldValue = $ventilationUnit->getFanSpeedLevel(false);
+$ventilationUnit->setFanSpeedLevel(1);
+echo "Set FanSpeedLevel to: ".$ventilationUnit->getFanSpeedLevel(true).PHP_EOL;
+$ventilationUnit->setFanSpeedLevel($oldValue);
+
+$oldValue = $ventilationUnit->getFilterDefaultTime();
+$ventilationUnit->setFilterDefaultTime(77);
+echo "Set FilterDefaultTime to: ".$ventilationUnit->getFilterDefaultTime(true).PHP_EOL;
+$ventilationUnit->setFilterDefaultTime($oldValue);
+
+$oldValue = $ventilationUnit->getUnitMode(false);
+$ventilationUnit->setUnitMode(16);
+echo "Set UnitMode to: ".$ventilationUnit->getUnitMode(true).PHP_EOL;
+$ventilationUnit->setUnitMode($oldValue);
+
+
 
 echo PHP_EOL;
--- src/VentilationUnit.php
+++ src/VentilationUnit.php
@@ -35,25 +35,25 @@ class VentilationUnit
         $modbus = new ModbusMasterTcp($ipAddress);
 
         // genereate register
-        $this->register['CurrentDateTime']       = new DateTime(         $modbus, '108', '40109', 'prmDateTime', 'Current date/time', 'date-time-format');
-        $this->register['StartExploitationDate'] = new DateTime(         $modbus, '668', '40669', 'prmStartExploitationDateStamp', 'date/time of the system start', 'date-time-format');
-        $this->register['WorkTime']              = new Numeric(          $modbus, '624', '40625', 'prmWorkTime', 'Work time of system, in hours', '%s hours');
-        $this->register['OutdoorTemperature']    = new Floating(         $modbus, '132', '40133', 'prmRamIdxT1', 'Outdoor temperature T1', '%s °C');
-        $this->register['SupplyTemperature']     = new Floating(         $modbus, '134', '40135', 'prmRamIdxT2', 'Supply temperature T2', '%s °C');
-        $this->register['ExtractTemperature']    = new Floating(         $modbus, '136', '40137', 'prmRamIdxT3', 'Extract temperature T3', '%s °C');
-        $this->register['ExhaustTemperature']    = new Floating(         $modbus, '138', '40139', 'prmRamIdxT4', 'Exhaust temperature T4', '%s °C');
-        $this->register['FanSpeedLevel']         = new FanSpeedLevel(    $modbus, '324', '40325', 'prmRomIdxSpeedLevel', 'Speed level of Fans', '%s');
-        $this->register['FanSpeed1']             = new Floating(         $modbus, '100', '40101', 'prmHALTaho1', 'Fan1 rpm', '%s rpm');
-        $this->register['FanSpeed2']             = new Floating(         $modbus, '102', '40103', 'prmHALTaho2', 'Fan2 rpm', '%s rpm');
-        $this->register['FilterDefaultTime']     = new FilterDefaultTime($modbus, '556', '40555', 'prmFilterDefaultTime', 'Filter Lifetime (Days)', '%s days');
-        $this->register['FilterRemainingTime']   = new Numeric(          $modbus, '554', '40555', 'prmFilterRemainingTime', 'Remaining time of the Filter Lifetime', '%s days');
-        $this->register['BypassTemperatureMin']  = new Floating(         $modbus, '444', '40445', 'prmBypassTmin', 'Min temperature for outdoor air', '%s °C');
-        $this->register['BypassTemperatureMax']  = new Floating(         $modbus, '446', '40447', 'prmBypassTmax', 'Max temperature for extract air', '%s °C');
-        $this->register['BypassManualTimeout']   = new Numeric(          $modbus, '264', '40265', 'prmRamIdxBypassManualTimeout', 'Manual bypass duration in minutes', '%s minutes');
-        $this->register['BypassState']           = new BypassState(      $modbus, '198', '40199', 'prmRamIdxBypassActualState', 'Bypass state');
-        $this->register['PreheaterDutyCycle']    = new Numeric(          $modbus, '160', '40161', 'prmPreheaterDutyCycle', 'Power of Preheater in %', '%s %%');
-        $this->register['CurrentBLState']        = new CurrentBLState(   $modbus, '472', '40473', 'prmCurrentBLState', 'Current unit mode');
-        $this->register['UnitMode']              = new UnitMode(         $modbus, '168', '40169', 'prmRamIdxUnitMode', 'Active Unit mode');
+        $this->register['CurrentDateTime'] = new DateTime($modbus, '108', '40109', 'prmDateTime', 'Current date/time', 'date-time-format');
+        $this->register['StartExploitationDate'] = new DateTime($modbus, '668', '40669', 'prmStartExploitationDateStamp', 'date/time of the system start', 'date-time-format');
+        $this->register['WorkTime'] = new Numeric($modbus, '624', '40625', 'prmWorkTime', 'Work time of system, in hours', '%s hours');
+        $this->register['OutdoorTemperature'] = new Floating($modbus, '132', '40133', 'prmRamIdxT1', 'Outdoor temperature T1', '%s °C');
+        $this->register['SupplyTemperature'] = new Floating($modbus, '134', '40135', 'prmRamIdxT2', 'Supply temperature T2', '%s °C');
+        $this->register['ExtractTemperature'] = new Floating($modbus, '136', '40137', 'prmRamIdxT3', 'Extract temperature T3', '%s °C');
+        $this->register['ExhaustTemperature'] = new Floating($modbus, '138', '40139', 'prmRamIdxT4', 'Exhaust temperature T4', '%s °C');
+        $this->register['FanSpeedLevel'] = new FanSpeedLevel($modbus, '324', '40325', 'prmRomIdxSpeedLevel', 'Speed level of Fans', '%s');
+        $this->register['FanSpeed1'] = new Floating($modbus, '100', '40101', 'prmHALTaho1', 'Fan1 rpm', '%s rpm');
+        $this->register['FanSpeed2'] = new Floating($modbus, '102', '40103', 'prmHALTaho2', 'Fan2 rpm', '%s rpm');
+        $this->register['FilterDefaultTime'] = new FilterDefaultTime($modbus, '556', '40555', 'prmFilterDefaultTime', 'Filter Lifetime (Days)', '%s days');
+        $this->register['FilterRemainingTime'] = new Numeric($modbus, '554', '40555', 'prmFilterRemainingTime', 'Remaining time of the Filter Lifetime', '%s days');
+        $this->register['BypassTemperatureMin'] = new Floating($modbus, '444', '40445', 'prmBypassTmin', 'Min temperature for outdoor air', '%s °C');
+        $this->register['BypassTemperatureMax'] = new Floating($modbus, '446', '40447', 'prmBypassTmax', 'Max temperature for extract air', '%s °C');
+        $this->register['BypassManualTimeout'] = new Numeric($modbus, '264', '40265', 'prmRamIdxBypassManualTimeout', 'Manual bypass duration in minutes', '%s minutes');
+        $this->register['BypassState'] = new BypassState($modbus, '198', '40199', 'prmRamIdxBypassActualState', 'Bypass state');
+        $this->register['PreheaterDutyCycle'] = new Numeric($modbus, '160', '40161', 'prmPreheaterDutyCycle', 'Power of Preheater in %', '%s %%');
+        $this->register['CurrentBLState'] = new CurrentBLState($modbus, '472', '40473', 'prmCurrentBLState', 'Current unit mode');
+        $this->register['UnitMode'] = new UnitMode($modbus, '168', '40169', 'prmRamIdxUnitMode', 'Active Unit mode');
     }
 
     /**
@@ -61,103 +61,102 @@ class VentilationUnit
      * @param bool $formatted
      * @return mixed
      */
-    protected function getValueByFunction($function, $formatted=false)
+    protected function getValueByFunction($function, $formatted = false)
     {
         $index = str_replace('get', '', $function);
         return $this->register[$index]->getValue($formatted);
     }
 
 
-
     /************************* GETTER SECTION *************************/
 
-    public function getCurrentDateTime($formatted=false)
+    public function getCurrentDateTime($formatted = false)
     {
         return $this->getValueByFunction(__FUNCTION__, $formatted);
     }
 
-    public function getStartExploitationDate($formatted=false)
+    public function getStartExploitationDate($formatted = false)
     {
         return $this->getValueByFunction(__FUNCTION__, $formatted);
     }
 
-    public function getWorkTime($formatted=false)
+    public function getWorkTime($formatted = false)
     {
         return $this->getValueByFunction(__FUNCTION__, $formatted);
     }
 
-    public function getOutdoorTemperature($formatted=false)
+    public function getOutdoorTemperature($formatted = false)
     {
         return $this->getValueByFunction(__FUNCTION__, $formatted);
     }
 
 
-    public function getSupplyTemperature($formatted=false)
+    public function getSupplyTemperature($formatted = false)
     {
         return $this->getValueByFunction(__FUNCTION__, $formatted);
     }
 
-    public function getExtractTemperature($formatted=false)
+    public function getExtractTemperature($formatted = false)
     {
         return $this->getValueByFunction(__FUNCTION__, $formatted);
     }
 
-    public function getExhaustTemperature($formatted=false)
+    public function getExhaustTemperature($formatted = false)
     {
         return $this->getValueByFunction(__FUNCTION__, $formatted);
     }
 
-    public function getFanSpeed1($formatted=false)
+    public function getFanSpeed1($formatted = false)
     {
         return $this->getValueByFunction(__FUNCTION__, $formatted);
     }
 
-    public function getFanSpeed2($formatted=false)
+    public function getFanSpeed2($formatted = false)
     {
         return $this->getValueByFunction(__FUNCTION__, $formatted);
     }
 
-    public function getFanSpeedLevel($formatted=false)
+    public function getFanSpeedLevel($formatted = false)
     {
         return $this->getValueByFunction(__FUNCTION__, $formatted);
     }
 
-    public function getFilterDefaultTime($formatted=false)
+    public function getFilterDefaultTime($formatted = false)
     {
         return $this->getValueByFunction(__FUNCTION__, $formatted);
     }
 
-    public function getBypassTemperatureMin($formatted=false)
+    public function getBypassTemperatureMin($formatted = false)
     {
         return $this->getValueByFunction(__FUNCTION__, $formatted);
     }
 
-    public function getBypassTemperatureMax($formatted=false)
+    public function getBypassTemperatureMax($formatted = false)
     {
         return $this->getValueByFunction(__FUNCTION__, $formatted);
     }
 
-    public function getBypassManualTimeout($formatted=false)
+    public function getBypassManualTimeout($formatted = false)
     {
         return $this->getValueByFunction(__FUNCTION__, $formatted);
     }
 
-    public function getBypassState($formatted=false)
+    public function getBypassState($formatted = false)
     {
         return $this->getValueByFunction(__FUNCTION__, $formatted);
     }
 
-    public function getUnitMode($formatted=false)
+    public function getUnitMode($formatted = false)
     {
         return $this->getValueByFunction(__FUNCTION__, $formatted);
     }
 
-    public function getPreheaterDutyCycle($formatted=false)
+    public function getPreheaterDutyCycle($formatted = false)
     {
         return $this->getValueByFunction(__FUNCTION__, $formatted);
     }
 
-    public function getCurrentBLState($formatted=false)
+    public function getCurrentBLState($formatted = false)
     {
         return $this->getValueByFunction(__FUNCTION__, $formatted);
     }
@@ -185,6 +184,5 @@ class VentilationUnit
         $register = $this->register['FilterDefaultTime'];
         $register->writeValue($value);
     }
-
 
 }
